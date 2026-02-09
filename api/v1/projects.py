9c498e84b340113955fb5d2d50e0840ca6149622
Abhishek Tarun <abhishek.tarun09@gmail.com>
2026-02-10 00:43:41 +0100

    added api to view members in the project

--- api/v1/projects.py
+++ api/v1/projects.py
@@ -1,7 +1,7 @@
 from fastapi import Request, status, HTTPException, Depends, APIRouter
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession
-from api.v1.schemas.projects_schema import AddProjectsOut, AddProjectsIn, AddUsersIn, AddUsersOut, ListProjects, UpdateProjectsIn, UpdateProjectsOut
+from api.v1.schemas.projects_schema import AddProjectsOut, AddProjectsIn, AddUsersIn, AddUsersOut, ListMembers, ListProjects, UpdateProjectsIn, UpdateProjectsOut
 from database.models.organization_member import OrganizationMember
 from database.models.project_member import ProjectMember
 from database.models.projects import Project
@@ -255,3 +255,39 @@ async def list_projects(db: AsyncSession = Depends(get_db), current_user_and_mem
     ]
     
     return {"project_details" : project_details}
+
+@router.get("/members/{project_id}", response_model=ListMembers, status_code=status.HTTP_200_OK)
+async def list_members(project_id: int, db: AsyncSession = Depends(get_db), current_user_and_membership = Depends(get_user_and_membership)):
+    
+    current_user, membership = current_user_and_membership
+    
+    if membership.role.value not in ("owner", "admin"):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to view project members")
+    
+    project = (
+        await db.execute(
+            select(Project).where(Project.organization_id == membership.organization_id, Project.id == project_id)
+        )
+    ).scalars().first()
+    
+    if not project:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Project not in Organization")
+    
+    members_in_project = (
+        await db.execute(
+            select(Users)
+            .join(ProjectMember, ProjectMember.user_id == Users.id)
+            .join(Project, Project.id == ProjectMember.project_id)
+            .where(Project.organization_id == membership.organization_id, Project.id == project_id)
+        )
+    ).scalars().all()
+    
+    if not members_in_project:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No member in the project")
+    
+    member_details = [
+        {"name": member.name, "email": member.email}
+        for member in members_in_project
+    ]
+    
+    return {"member_details" : member_details}
