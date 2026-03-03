e994b410bd323336ce4d07f4d637c7dd8fbf07ac
Abhishek Tarun <abhishek.tarun09@gmail.com>
2026-03-03 18:13:14 +0100

    added redis caching to /users/orgs

--- api/v2/users/list_orgs.py
+++ api/v2/users/list_orgs.py
@@ -1,4 +1,7 @@
+import json
+
 from fastapi import Depends, APIRouter
+from core.redis.redis_config import redis_client as redis
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.oauth2 import get_current_user
 from core.rate_limiter import RateLimiter
@@ -15,17 +18,29 @@ async def list_orgs(
     db: AsyncSession = Depends(get_db), current_user: int = Depends(get_current_user)
 ):
 
-    # List out the organizations the user is part of for frontend to select
-    org_ids = (
-        (
-            await db.execute(
-                select(OrganizationMember.organization_id).where(
-                    OrganizationMember.user_id == current_user.id
+    cache_key = f"user_id:{current_user.id}:/users/orgs"
+    cached_user = await redis.get(cache_key)
+
+    if cached_user:
+        cached_data = json.loads(cached_user)
+        return ListOrgs.model_validate(cached_data)
+
+    else:
+        # List out the organizations the user is part of for frontend to select
+        org_ids = (
+            (
+                await db.execute(
+                    select(OrganizationMember.organization_id).where(
+                        OrganizationMember.user_id == current_user.id
+                    )
                 )
             )
+            .scalars()
+            .all()
         )
-        .scalars()
-        .all()
-    )
 
-    return {"email": current_user.email, "org_ids": org_ids}
+        user_data = ListOrgs(email=current_user.email, org_ids=org_ids)
+
+        await redis.set(cache_key, user_data.model_dump_json(), ex=60 * 5)
+
+        return user_data
